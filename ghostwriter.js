54abd47f07e927ac5cc0ccc8e192bdd199ca7026
gking4 <gking4@mail.csuchico.edu>
2017-04-06 19:35:32 -0700

    Add files via upload

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,18 @@
 function writeTweet()
 {
-  
+  var randomIndex;
+  var sentence;
+  var intro = ["Whats up with ", "How does ", "Where did you get ", "When did "];
+  var body = ["that rock ", "that hat ", "the dining hall food ", "those bathrooms ", "the government "];
+  var end = ["learn how to drive a car?", "tame wild animals?"];
+  randIndex = randomUpTo(intro.length-1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length-1);
+  sentence += body[randIndex];
+  randIndex = randomUpTo(end.length-1);
+  sentence += end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
+
 }
 
 //Generates a random whole number between 0 and max (inclusive)
